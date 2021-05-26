b8feefcf7018973834e9019a51086f6fdd15cc87
Yucen Liu <yucen.u@gmail.com>
2021-05-26 15:31:22 +0000

    initial adding

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if(actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//** */ implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match 
+
+function eqArrays (array1,array2) {
+  let result = false;
+
+  if (array1.length != array2.length){
+    return false;
+  }
+  for(let i = 0; i < array1.length; i++){
+
+    if(array1[i] instanceof Array && array2[i] instanceof Array){
+      if(!array1[1].equals(array2[i]))
+      return false;
+    } 
+    else if (array1[i] != array2[i]){
+      return false;
+    }
+  }
+  return true;    
+}
+
+assertEqual(eqArrays(["1","2", "3", "4"], ["1","3", "3", "4"]), true);
+
