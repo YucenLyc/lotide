42f3b213230c233bba6676c5c53b2651ef5cffa3
Yucen Liu <yucen.u@gmail.com>
2021-05-27 02:22:37 +0000

    Formatting code

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if(actual === expected) {
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
@@ -9,23 +9,20 @@ const assertEqual = function(actual, expected) {
 // countLetter function: 
 // take in a sentence (as a string) and return a count of each of the letters in that sentence 
 
-function countLetter (sentence){
-  
+function countLetter(sentence) {
   const count = {};
 
-  for (let letter of sentence){
-    if(letter != " "){
-      if (count[letter]){
-        
+  for (let letter of sentence) {
+    if (letter != " ") {
+      if (count[letter]) {
         count[letter] += 1;
-
-    } else {
-
-      count[letter] = 1;
+      } else {
+        count[letter] = 1;
+      }
     }
   }
-}
-return count;
+
+  return count;
 }
 
 console.log(countLetter("command"));
