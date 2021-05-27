4537d7d8d9d4ae71570c53145ee3907d7ef94084
Yucen Liu <yucen.u@gmail.com>
2021-05-27 00:41:34 +0000

    initial adding

---
+++ countOnly.js
@@ -0,0 +1,27 @@
+
+
+//countOnly: given an array and an object, it will return an object containing counts of everything that the input object listed 
+
+//allItems: an array of strings that we need to look through 
+
+//itemsToCount: an object specifying what to count 
+
+const countOnly = function (allItems, itemsToCount){
+
+  const results = {};
+
+  for (const item of allItems){
+
+    if (itemsToCount[item]){
+
+    if(results[item]){
+
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+}
+  return results;
+}
+
