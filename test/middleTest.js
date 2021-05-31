be955431937d7a72d544479ec29523028cb79634
Yucen Liu <yucen.u@gmail.com>
2021-05-31 22:13:55 +0000

    inital adding of test for middle.js

---
+++ test/middleTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+middle([1,2,3,4,5,6,7,7,5,6,7,8,6]);
