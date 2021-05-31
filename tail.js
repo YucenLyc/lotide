6bebbd9c5eaa692bd86369adc6eb017ec4e0f04d
Yucen Liu <yucen.u@gmail.com>
2021-05-31 19:51:41 +0000

    initial commit- adding test code for tail.js

--- tail.js
+++ tail.js
@@ -6,21 +6,15 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
 
 //returning a new array and not modify the original array that is passed in
 function tail(array){
-  const length = array == null? 0: array.length
+  const length = array === null? 0: array.length
   if(! length){
     return []
   }
   const [, ...result] = array
   return result 
-}
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+};
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
